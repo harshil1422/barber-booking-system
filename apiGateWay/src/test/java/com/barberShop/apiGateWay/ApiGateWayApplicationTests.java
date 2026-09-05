b366488ae7a8ee610ae860b04c8e1bbd3f1361ad
Harshil Patel <harshilpatel3096@gmail.com>
2026-09-05 12:51:14 +0530

    removing eureka from api gateway

--- apiGateWay/src/test/java/com/barberShop/apiGateWay/ApiGateWayApplicationTests.java
+++ apiGateWay/src/test/java/com/barberShop/apiGateWay/ApiGateWayApplicationTests.java
@@ -2,14 +2,13 @@ package com.barberShop.apiGateWay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaAutoServiceRegistration;
+
 
 @SpringBootTest
 class ApiGateWayApplicationTests {
 
-	@MockBean
-	private EurekaAutoServiceRegistration eurekaAutoServiceRegistration;
+//	@MockBean
+//	private EurekaAutoServiceRegistration eurekaAutoServiceRegistration;
 	@Test
 	void contextLoads() {
 	}
